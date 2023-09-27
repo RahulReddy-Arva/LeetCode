674baa9aee554d26d1b9f370ab6f65e06101ad60
Rahul Reddy Arva <rahul@findem.ai>
2023-09-26 20:42:24 -0700

    [EASY] Valid Paranthesis (RT: 60%, Mem: 99.7%)

---
+++ Easy/valid_paranthesis.py
@@ -0,0 +1,50 @@
+###
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+# Example 1:
+# Input: s = "()"
+# Output: true
+
+# Example 2:
+# Input: s = "()[]{}"
+# Output: true
+
+# Example 3:
+# Input: s = "(]"
+# Output: false
+
+# Constraints:
+# 1 <= s.length <= 104
+
+# s consists of parentheses only '()[]{}'.
+###
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 0:
+            return False
+        if len(s) == 1:
+            return False
+
+        stk = []
+        for i in s:
+          if i in "({[":
+            stk.append(i)
+          else:
+            if len(stk) != 0 and ((i == ")" and stk[-1] == "(") or (i == "}" and stk[-1] == "{") or ( i == "]" and stk[-1] == "[")):
+              stk.pop()
+            else:
+              return False
+        if len(stk) == 0:
+          return True
+        return False
+
+s = Solution()
+inp = "(){}}{"
+p = s.isValid(s=inp)
+print(p)
